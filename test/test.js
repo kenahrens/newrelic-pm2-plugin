5f269f2ab06106dd1fab1cf585a7ac5425f5ff82
Ken Ahrens <ken.ahrens@gmail.com>
2016-04-14 16:37:58 -0400

    adding mocha

---
+++ test/test.js
@@ -0,0 +1,17 @@
+var app = require('../app.js');
+var assert = require('assert');
+
+describe('newrelic-pm2-plugin', function() {
+  before(function(){
+
+  });
+  beforeEach(function(){
+
+  });
+  it('loads', function() {
+
+  });
+  after(function() {
+
+  });
+})

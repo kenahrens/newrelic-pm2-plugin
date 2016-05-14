598ab0b379305da57af465c1caebd762026ac155
kenahrens <ken.ahrens@gmail.com>
2016-05-14 09:27:54 -0400

    fixing test

--- test/test.js
+++ test/test.js
@@ -1,12 +1,15 @@
 var app = require('../pm2plugin.js');
 var assert = require('assert');
+var fs = require('fs');
 
 describe('newrelic-pm2-plugin', function() {
   before(function(){
-
-  });
-  beforeEach(function(){
-
+    try {
+      var config = require('../config.json');
+    } catch (e) {
+      // Create a temp config.json
+      fs.createReadStream('../config-template.json').pipe(fs.createWriteStream('../config.json'));
+    }
   });
   it('loads', function() {
 

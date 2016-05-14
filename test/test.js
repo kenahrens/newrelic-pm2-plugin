1da2824349eaf79e8b345f586c4a979ae90fb80f
kenahrens <ken.ahrens@gmail.com>
2016-05-14 09:32:33 -0400

    adding logging to test

--- test/test.js
+++ test/test.js
@@ -5,10 +5,14 @@ var fs = require('fs');
 describe('newrelic-pm2-plugin', function() {
   before(function(){
     try {
+      console.log('checking for config.json');
       var config = require('../config.json');
+      console.log('looks like config.json already exists');
     } catch (e) {
       // Create a temp config.json
+      console.log('creating a temporary config.json for testing');
       fs.createReadStream('../config-template.json').pipe(fs.createWriteStream('../config.json'));
+      console.log('temporary config.json created');
     }
   });
   it('loads', function() {
